e30dd92fb08ddc3e8d27f6118d274a82ce8287e1
gcatanese <gcatanese@yahoo.com>
2020-02-19 22:38:09 +0100

    Use env variables

--- s3_simple_browser/s3_mgr.py
+++ s3_simple_browser/s3_mgr.py
@@ -1,14 +1,19 @@
 import boto3
+import os
 
 def get_client():
 
     session = boto3.session.Session()
 
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    endpoint_url = os.getenv('ENDPOINT_URL')
+
     s3 = session.client(
         service_name='s3',
-        aws_access_key_id='aws01',
-        aws_secret_access_key='aws01',
-        endpoint_url='http://localhost:4572'
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        endpoint_url=endpoint_url
     )
 
     return s3
