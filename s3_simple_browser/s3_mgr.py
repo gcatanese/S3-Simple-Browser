afc609dec5a2bd354b4f2dee9035e2a605c8a5f9
gcatanese <gcatanese@yahoo.com>
2020-02-20 14:19:45 +0100

    Support S3 and localstack connections

--- s3_simple_browser/s3_mgr.py
+++ s3_simple_browser/s3_mgr.py
@@ -1,19 +1,29 @@
 import boto3
 import os
 
-def get_client():
 
+def get_client():
     session = boto3.session.Session()
 
     aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
     aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-    endpoint_url = os.getenv('ENDPOINT_URL')
+    s3_localstack_url = os.getenv('S3_LOCALSTACK_URL')
 
-    s3 = session.client(
-        service_name='s3',
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        endpoint_url=endpoint_url
-    )
+    print(s3_localstack_url)
+
+    if s3_localstack_url is None:
+        s3 = session.client(
+            service_name='s3',
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key
+        )
+
+    else:
+        s3 = session.client(
+            service_name='s3',
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+            endpoint_url=s3_localstack_url
+        )
 
     return s3
