5059d35a1532f9f8103786723650bddf3e939f22
gcatanese <gcatanese@yahoo.com>
2020-02-20 15:58:24 +0100

    Fix url mapping

--- s3_simple_browser/urls.py
+++ s3_simple_browser/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('bucket/<str:bucket_name>', views.bucket, name='bucket'),
-    path('container/<str:bucket_name>/view', views.root, name='container'),
+    path('container/<str:bucket_name>/view/', views.root, name='container'),
     path('container/<str:bucket_name>/view/<str:object_key>', views.container, name='container'),
     path('container/<str:bucket_name>/delete/<str:object_key>', views.delete, name='message'),
     path('container/<str:bucket_name>/download/<str:object_key>', views.download, name='message'),
